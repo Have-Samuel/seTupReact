2a4f83786eca47ef2bc9eebeffb034ddfffea163
Have Samuel <samiehave9@gmail.com>
2022-04-21 03:00:33 +0300

    Added the buttons file

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,83 +1,93 @@
-/* eslint-disable indent */
+/* eslint-disable import/no-duplicates */
 import React from 'react';
 import './calculator.css';
+import './button';
+import calculate from './logic/calculate';
+import Buttons from './button';
 
 export default class Calculator extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      calcObj: { total: null, next: null, operation: null },
+      display: null,
+    };
+    this.handleClick = this.handleClick.bind(this);
+    this.buttons = [
+      'Ac',
+      '+/-',
+      '%',
+      '÷',
+      '7',
+      '8',
+      '9',
+      'x',
+      '4',
+      '5',
+      '6',
+      '-',
+      '1',
+      '2',
+      '3',
+      '+',
+      '0',
+      '.',
+      '=',
+    ];
+  }
+
+  handleDisplayState(beforeCalObj, afterCalcObj) {
+    // handle display UI state
+    if (!afterCalcObj.operation && !afterCalcObj.next && afterCalcObj.total) {
+      this.setState({ display: afterCalcObj.total });
+    } else if (beforeCalObj.operation && afterCalcObj.next) {
+      this.setState({ display: afterCalcObj.next });
+    } else if (afterCalcObj.total && afterCalcObj.operation) {
+      this.setState({ display: afterCalcObj.total });
+    } else if (!afterCalcObj.operation) {
+      this.setState({ display: afterCalcObj.next });
+    } else {
+      this.setState({ display: 0 });
+    }
+  }
+
+  handleClick(e) {
+    const { calcObj } = this.state;
+    const newCalcObj = calculate(calcObj, e.target.innerText);
+    this.setState({ calcObj: newCalcObj });
+    this.handleDisplayState(calcObj, newCalcObj);
+  }
+
   render() {
+    const { display, calcObj } = this.state;
     return (
       <div className="calculator">
-        <div className="display">0</div>
+        <div className="display">{display ?? calcObj.next ?? 0}</div>
         <div className="keyboard">
           <div className="keyboard-row">
-            <button type="button" className="keyboardBtn">
-              AC
-            </button>
-            <button type="button" className="keyboardBtn">
-              +/-
-            </button>
-            <button type="button" className="keyboardBtn">
-              %
-            </button>
-            <button type="button" className="keyboardBtn">
-              &divide;
-            </button>
+            {this.buttons.slice(0, 4).map((btn) => (
+              <Buttons key={btn} name={btn} handleClick={this.handleClick} />
+            ))}
           </div>
           <div className="keyboard-row">
-            <button type="button" className="keyboardBtn">
-              7
-            </button>
-            <button type="button" className="keyboardBtn">
-              8
-            </button>
-            <button type="button" className="keyboardBtn">
-              9
-            </button>
-            <button type="button" className="keyboardBtn">
-              &times;
-            </button>
+            {this.buttons.slice(4, 8).map((btn) => (
+              <Buttons key={btn} name={btn} handleClick={this.handleClick} />
+            ))}
           </div>
           <div className="keyboard-row">
-            <button type="button" className="keyboardBtn">
-              4
-            </button>
-            <button type="button" className="keyboardBtn">
-              5
-            </button>
-            <button type="button" className="keyboardBtn">
-              6
-            </button>
-            <button type="button" className="keyboardBtn">
-              -
-            </button>
+            {this.buttons.slice(8, 12).map((btn) => (
+              <Buttons key={btn} name={btn} handleClick={this.handleClick} />
+            ))}
           </div>
           <div className="keyboard-row">
-            <button type="button" className="keyboardBtn">
-              1
-            </button>
-            <button type="button" className="keyboardBtn">
-              2
-            </button>
-            <button type="button" className="keyboardBtn">
-              3
-            </button>
-            <button type="button" className="keyboardBtn">
-              +
-            </button>
+            {this.buttons.slice(12, 16).map((btn) => (
+              <Buttons key={btn} name={btn} handleClick={this.handleClick} />
+            ))}
           </div>
           <div className="keyboard-row">
-            <button
-              id="null"
-              type="button"
-              className="keyboardBtn"
-            >
-              0
-            </button>
-            <button type="button" className="keyboardBtn">
-              .
-            </button>
-            <button type="button" className="keyboardBtn">
-              =
-            </button>
+            {this.buttons.slice(16, 19).map((btn) => (
+              <Buttons key={btn} name={btn} handleClick={this.handleClick} />
+            ))}
           </div>
         </div>
       </div>
